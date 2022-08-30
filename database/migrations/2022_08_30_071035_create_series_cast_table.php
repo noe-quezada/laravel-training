aff01cd3c5ff00eaa28d3fb8436f29c133946a8a
noe-quezada <noequesada@folio3.com>
2022-08-30 02:37:50 -0500

    migrations created

---
+++ database/migrations/2022_08_30_071035_create_series_cast_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series_cast', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cast_id')->references('id')->on('cast');
+            $table->foreignId('series_id')->constrained('series');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('series_cast');
+    }
+};
---
+++ database/migrations/2022_08_30_071043_create_cast_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cast', function (Blueprint $table) {
+            $table->unsignedBigInteger('id');
+            $table->string('name', 25);
+            $table->string('last_name', 15);
+            $table->date('birthday');
+            $table->string('nationality', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cast');
+    }
+};
